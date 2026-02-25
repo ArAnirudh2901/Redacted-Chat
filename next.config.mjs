7d5b5b9229e020cecf60997705138c7e847102e9
Anirudh Aravalli <anirudharavalli@Anirudhs-MacBook-Air.local>
2026-02-25 11:14:22 +0530

    chore: remove obsolete eslint config from next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,11 +5,7 @@ const nextConfig = {
   serverExternalPackages: [
     "sharp",
   ],
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+
   typescript: {
     ignoreBuildErrors: true,
   },
